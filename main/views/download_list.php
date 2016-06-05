6dc27af179d7c8e801bb2b09e9ccefd31f93ed8b
Vin Wong <vin89423@gmail.com>
2016-06-05 18:57:20 +0800

    Update download_list.php

--- main/views/download_list.php
+++ main/views/download_list.php
@@ -1,26 +1,17 @@
 <div id="no-downloads">
-
-
-
 	<div class="valign-wrapper">
 		<div class="valign">
 			<img src="<?php echo $URL_RSC; ?>img/no_download.png" />
-			<p>這裡沒有任何內容...</p>
+			<p><?php echo $this->lang('no_download_item'); ?>...</p>
 		</div>
 	</div>
-	
-	
-	
 </div>
-
 <div id="download-list"></div>
-
 <div class="fixed-action-btn">
 	<a class="btn-floating btn-large red" data-event="add-request">
 		<i class="material-icons">mode_edit</i>
 	</a>
 </div>
-
 <div id="item-template" style="display: none;">
 	<div class="item row">
 		<div class="col s12">
@@ -40,41 +31,40 @@
 						<div class="progress" style="display: none;">
 							<div class="determinate" style="width: 0%"></div>
 						</div>
-						<a href="#" data-event="download" style="display: none;">下載到本機</a>
-						<a href="#" data-event="retry" style="display: none;">重試</a>
-						<!--<a href="#" data-event="cancel" style="display: none;">取消下載</a>-->
+						<a href="#" data-event="download" style="display: none;"><?php echo $this->lang('download_to_local'); ?></a>
+						<a href="#" data-event="retry" style="display: none;"><?php echo $this->lang('retry'); ?></a>
+						<!--<a href="#" data-event="cancel" style="display: none;"><?php echo $this->lang('cancel_download'); ?></a>-->
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>	
 </div>
-
 <div id="request-modal" class="modal">
 	<div class="modal-content">
-		<h4>建立下載計劃</h4>
+		<h4><?php echo $this->lang('create_download_request'); ?></h4>
 		<div class="input-field col s12">
 			<input id="url" type="url" class="validate">
-			<label for="url" data-error="連結不能是空的">網址連結</label>
+			<label for="url" data-error="<?php echo $this->lang('url_link_cannot_blank'); ?>"><?php echo $this->lang('url_link'); ?></label>
 		</div>
 		<div class="input-field col s12">
 			<input id="filename" type="text" class="validate">
-			<label for="filename">檔案名稱</label>
+			<label for="filename"><?php echo $this->lang('filename'); ?></label>
 		</div>
 	</div>
 	<div class="modal-footer">
-		<a href="#!" class="modal-action waves-effect btn-flat" data-event="cancel">取消</a>
-		<a href="#!" class=" modal-action waves-effect btn-flat" data-event="download">確認下載</a>
+		<a href="#!" class="modal-action waves-effect btn-flat" data-event="cancel"><?php echo $this->lang('cancel'); ?></a>
+		<a href="#!" class=" modal-action waves-effect btn-flat" data-event="download"><?php echo $this->lang('confirm_download'); ?></a>
 	</div>
 </div>
 
 <div id="remove-modal" class="modal">
 	<div class="modal-content">
-		<p>你確要刪除這個項目嗎?</p>
+		<p><?php echo $this->lang('confirm_to_delete_ask') ?>?</p>
 	</div>
 	<div class="modal-footer">
-		<a href="#!" class="modal-action waves-effect btn-flat" data-event="cancel">取消</a>
-		<a href="#!" class="modal-action waves-effect btn-flat" data-event="remove" data-signature="">確認刪除</a>
+		<a href="#!" class="modal-action waves-effect btn-flat" data-event="cancel"><?php echo $this->lang('cancel'); ?></a>
+		<a href="#!" class="modal-action waves-effect btn-flat" data-event="remove" data-signature=""><?php echo $this->lang('confirm_to_delete'); ?></a>
 	</div>
 </div>
 
