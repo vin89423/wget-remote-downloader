a985ee7c170c550e2ed9f0d3963a23e3ea513762
Vin Wong <vin89423@gmail.com>
2016-06-05 18:57:38 +0800

    Update login.php

--- main/views/login.php
+++ main/views/login.php
@@ -5,20 +5,20 @@
 				<div class="row">
 					<div class="input-field col s12">
 						<input id="username" type="text" class="validate" required>
-						<label for="username" data-error="用戶名不能是空的">用戶名</label>
+						<label for="username" data-error="<?php echo $this->lang('username_cannot_blank') ?>"><?php echo $this->lang('username'); ?></label>
 					</div>
 				</div>
 				<div class="row">
 					<div class="input-field col s12">
 						<input id="password" type="password" class="validate" required>
-						<label for="password" data-error="請輸入密碼">密碼</label>
+						<label for="password" data-error="<?php echo $this->lang('password_cannot_blank') ?>"><?php echo $this->lang('password'); ?></label>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col s12">
 						<p>
 							<input type="checkbox" id="remember">
-							<label for="remember">保持登入</label>
+							<label for="remember"><?php echo $this->lang('keep_login'); ?></label>
 						</p>
 					</div>
 				</div>
@@ -26,7 +26,7 @@
 				<div class="row">
 					<div class="col m12">
 						<p class="right-align">
-							<button class="btn btn-large waves-effect waves-light" type="button" data-event="login">登入</button>
+							<button class="btn btn-large waves-effect waves-light" type="button" data-event="login"><?php echo $this->lang('login'); ?></button>
 						</p>
 					</div>
 				</div>
@@ -35,6 +35,9 @@
 	</div>		
 </div>
 <script>
+$.lang(<?php echo json_encode(array(
+	'username_not_exist' => $this->lang('username_not_exist'),
+)); ?>)
 $(function(){
 	DOWNLOAD.initLogin();
 });
