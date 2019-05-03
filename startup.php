1d370770311ef51dc6a2115be1d5e038635fdb7b
Vin WONG <vin89423@gmail.com>
2019-05-03 21:07:17 +0800

    update config

--- startup.php
+++ startup.php
@@ -9,16 +9,18 @@ $START_UP = array(
 	// For basic member authorization. [NECESSARY]
 	'session' => array(
 		'token' => 'RD_TOKEN',
-		'encrypt' => '{encrypt-key}',
+		'key' => '{session_key}',
+		'iv' => '{session_iv}',
 	),
 	// Application routing. [NECESSARY]
 	'application' => array(
 		'activity' => array(
 			'main' => array(
 				'launch' => 'MainApp',
+				'storage' => '{storage-abs-path}',
 				'languageSource' => 'ini',
 				'language' => array('en', 'zt'),
-			)
+			),
 		),
-	)
+	),
 );
